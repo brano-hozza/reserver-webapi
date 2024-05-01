342a2afd885632ab83b6254fde5b706d496207de
Branislav Hozza <brankohozza@gmail.com>
2024-05-01 15:00:18 +0200

    Add mongodb compose file

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -10,10 +10,25 @@ $ProjectRoot = "${PSScriptRoot}/.."
 
 $env:RESERVER_API_ENVIRONMENT = "Development"
 $env:RESERVER_API_PORT = "8080"
+$env:RESERVER_API_MONGODB_USERNAME = "root"
+$env:RESERVER_API_MONGODB_PASSWORD = "neUhaDnes"
+
+function mongo {
+    docker compose --file ${ProjectRoot}/deployments/docker-compose/compose.yaml $args
+}
 
 switch ($command) {
     "start" {
-        go run ${ProjectRoot}/cmd/reserver-api-service
+        try {
+            mongo up --detach
+            go run ${ProjectRoot}/cmd/reserver-api-service
+        }
+        finally {
+            mongo down
+        }
+    }
+    "mongo" {
+        mongo up
     }
     "openapi" {
         docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
